817053e884ba4ebc8be4a0785ec36cee8ef20133
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-23 16:09:08 -0300

    Card adicionado no App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <Form />
-      </div>
+        <Card />
+      </>
     );
   }
 }
