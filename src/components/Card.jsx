93b1a98a9d877ce85794264f0c35ec022d7e6860
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-23 16:11:44 -0300

    Adicionando props no Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 export default class Card extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
     return (
       <div>Card</div>
     );
