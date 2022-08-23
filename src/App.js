5471e52b7657a8518c9de4a8ba88f1a06627f437
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-23 17:02:27 -0300

    Valores adicionado no car e Requisito 4 concluido

--- src/App.js
+++ src/App.js
@@ -6,21 +6,66 @@ class App extends React.Component {
   state = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: '',
-    cardAttr2: '',
-    cardAttr3: '',
+    cardAttr1: 0,
+    cardAttr2: 0,
+    cardAttr3: 0,
     cardImage: '',
     cardRare: '',
     cardTrunfo: false,
-    hasTrunfo: false,
+    // hasTrunfo: false,
     isSaveButtonDisabled: true,
   };
 
+  handleChande = ({ target }) => {
+    const { type, name } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+
+    this.setState(({
+      [name]: value,
+    }));
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onSaveButtonClick,
+    } = this.state;
     return (
       <>
-        <Form />
-        <Card />
+        <Form
+          onInputChange={ this.handleChande }
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          // hasTrunfo,
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ onSaveButtonClick }
+        />
+        <Card
+          onInputChange={ this.handleChande }
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </>
     );
   }
--- src/components/preview.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class preview extends Component {
-  render() {
-    return (
-      <div>preview</div>
-    );
-  }
-}
