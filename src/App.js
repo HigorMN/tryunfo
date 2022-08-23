0bbc6a11a1be1bdcb9782b4dbe664bd097650a6a
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-23 16:41:53 -0300

    estados criados

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+    hasTrunfo: false,
+    isSaveButtonDisabled: true,
+  };
+
   render() {
     return (
       <>
---
+++ src/components/preview.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class preview extends Component {
+  render() {
+    return (
+      <div>preview</div>
+    );
+  }
+}
