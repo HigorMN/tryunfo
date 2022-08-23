0f32bd36c7117bf66b0631f1d5dcd03bfc32af5e
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-23 19:12:02 -0300

    Refatorando a 5

--- src/App.js
+++ src/App.js
@@ -22,40 +22,44 @@ class App extends React.Component {
 
     this.setState(({
       [name]: value,
-    }), () => {
-      const {
-        cardName,
-        cardDescription,
-        cardImage,
-        cardRare,
-        cardAttr1,
-        cardAttr2,
-        cardAttr3,
-      } = this.state;
-      const max = 90;
-      const valor1 = !!(Number(cardAttr1) >= 0 && Number(cardAttr1) <= max);
-      const valor2 = !!(Number(cardAttr2) >= 0 && Number(cardAttr2) <= max);
-      const valor3 = !!(Number(cardAttr3) >= 0 && Number(cardAttr3) <= max);
-      const valorTotal = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
-      const vlMin = 210;
-      if (cardName
-        && cardDescription
-        && cardImage
-        && cardRare
-        && valor1
-        && valor2
-        && valor3
-        && valorTotal <= vlMin
-      ) {
-        this.setState({
-          isSaveButtonDisabled: false,
-        });
-      } else {
-        this.setState({
-          isSaveButtonDisabled: true,
-        });
-      }
-    });
+    }), this.validation);
+  };
+
+  validation = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const max = 90;
+    const valor1 = !!(Number(cardAttr1) >= 0 && Number(cardAttr1) <= max);
+    const valor2 = !!(Number(cardAttr2) >= 0 && Number(cardAttr2) <= max);
+    const valor3 = !!(Number(cardAttr3) >= 0 && Number(cardAttr3) <= max);
+    const valorTotal = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const vlMin = 210;
+
+    if (cardName
+      && cardDescription
+      && cardImage
+      && cardRare
+      && valor1
+      && valor2
+      && valor3
+      && valorTotal <= vlMin
+    ) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   };
 
   render() {
